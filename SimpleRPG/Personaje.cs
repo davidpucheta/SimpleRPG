ab25a3bcfa755fc26a283676733811d10f638cab
David Reyes Pucheta <davidpucheta@gmail.com>
2016-11-30 17:28:47 -0600

    Crear Clase Personaje

---
+++ SimpleRPG/Personaje.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace SimpleRPG
+{
+	public class Personaje
+	{
+		public string nombre { get; set; }
+
+		public void atacar(int arma)
+		{
+			Console.WriteLine( nombre +  " " + usar(arma));
+		}
+
+
+		public string usar(int item)
+		{
+			string itemUsado = "";
+			switch (item)
+			{
+				case 1:
+					itemUsado = "ha partido en dos al enemigo";
+					break;
+				case 2:
+					itemUsado = "ha acertado en el corazón del enemigo";
+					break;
+				case 3:
+					itemUsado = "ha convertido en mono al enemigo";
+					break;
+				default:
+					Console.WriteLine("Game Over");
+					break;
+			}
+			return itemUsado;
+		}
+	}
+}
--- SimpleRPG/Program.cs
+++ SimpleRPG/Program.cs
@@ -7,12 +7,42 @@ namespace SimpleRPG
 		public static void Main(string[] args)
 		{
 			int seleccion = 0;
+			int arma = 0;
 
 			do
 			{
 				seleccion = PintarMenuPersonaje();
+				if (seleccion == 0) 
+				{
+					Console.WriteLine("Game Over");
+					return;
+				}
 
-			} while (seleccion != 0);
+				arma = PintarMenuItems(); 
+
+				switch (seleccion)
+				{
+					case 1:
+						Personaje conan = new Personaje();
+						conan.nombre = "Conan";
+						conan.atacar(arma);
+						break;
+					case 2:
+						Personaje legolas = new Personaje();
+						legolas.nombre = "Légolas";
+						legolas.atacar(arma);
+						break;
+					case 3:
+						Personaje merlin = new Personaje();
+						merlin.nombre = "Merlín";
+						merlin.atacar(arma);
+						break;
+					default:
+						Console.WriteLine("Game Over");
+						break;
+				}
+
+			} while (seleccion > 0 && seleccion < 4);
 		}
 
 		public static int PintarMenuPersonaje()
@@ -22,10 +52,30 @@ namespace SimpleRPG
 			Console.WriteLine("1. Paladín");
 			Console.WriteLine("2. Elfo");
 			Console.WriteLine("3. Mago");
-			Console.WriteLine("0. Salir del Programa");
 
-			var result = Console.ReadLine();
-			return Convert.ToInt32(result);
+			Console.WriteLine("Selecciona cualquier otra número para salir");
+			Console.WriteLine();
+
+
+			var personaje = Console.ReadLine();
+			return Convert.ToInt32(personaje);
+		}
+
+		public static int PintarMenuItems()
+		{
+			Console.WriteLine("Selecciona un Arma:");
+			Console.WriteLine();
+			Console.WriteLine("1. Espada");
+			Console.WriteLine("2. Arco");
+			Console.WriteLine("3. Hechizo");
+
+			Console.WriteLine("Cualquier otra número para salir");
+			Console.WriteLine();
+
+
+			var arma = Console.ReadLine();
+			return Convert.ToInt32(arma);
+			
 		}
 
 	}
